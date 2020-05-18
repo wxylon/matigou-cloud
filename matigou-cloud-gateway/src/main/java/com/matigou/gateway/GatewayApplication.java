97a053ae39fffb8ced1a4c8703aff6e3b071254b
wangxiong <wxylon@gmail.com>
2020-05-18 22:17:37 +0800

    将 jar包部署方式 修改为 war包部署方式

--- matigou-cloud-gateway/src/main/java/com/matigou/gateway/GatewayApplication.java
+++ matigou-cloud-gateway/src/main/java/com/matigou/gateway/GatewayApplication.java
@@ -3,12 +3,19 @@ package com.matigou.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GatewayApplication {
+public class GatewayApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GatewayApplication.class);
     }
 }
--- matigou-cloud-web/src/main/java/com/matigou/web/MatigouApplication.java
+++ matigou-cloud-web/src/main/java/com/matigou/web/MatigouApplication.java
@@ -2,18 +2,23 @@ package com.matigou.web;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-@EnableAutoConfiguration
-public class MatigouApplication {
+public class MatigouApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MatigouApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MatigouApplication.class);
     }
 }
