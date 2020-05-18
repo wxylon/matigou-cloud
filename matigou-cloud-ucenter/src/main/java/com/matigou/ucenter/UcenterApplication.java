ec7eb75efbc391c5b3247c31730cf2c17e7b9d76
wangxiong <wxylon@gmail.com>
2020-05-18 18:28:27 +0800

    将 jar包部署方式 修改为 war包部署方式

--- matigou-cloud-ucenter/src/main/java/com/matigou/ucenter/UcenterApplication.java
+++ matigou-cloud-ucenter/src/main/java/com/matigou/ucenter/UcenterApplication.java
@@ -1,14 +1,20 @@
 package com.matigou.ucenter;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UcenterApplication {
+public class UcenterApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(UcenterApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(UcenterApplication.class);
     }
 }
